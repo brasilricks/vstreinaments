b7b92f29924f2ea4611c177d22493f97697c821d
Ricardo Alcantara da Silva <brasilricks@hotmail.com>
2017-08-29 23:38:26 -0300

    Teste de Git

---
+++ testegit1/testegit1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testegit1
+{
+    class calculadora 
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int soma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
